ee7b86cc54400d68337f38037ff9f1cd9c5eff64
Jason Macnak <natsu@google.com>
2022-12-12 16:03:54 +0000

    Revert "ANDROID: Temporarily add logging to debug b/249457381"
    This reverts commit f08622ae72b16f3ccf6a8bfa5348587180476e86.
    
    Reason for revert: No longer needed to investigate b/249457381
    
    Change-Id: I8780eabf7fa7fa99120f45edee8d45fad69a46fa

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -1344,7 +1344,6 @@ bool RendererVk::hasSharedGarbage()
 
 void RendererVk::onDestroy(vk::Context *context)
 {
-    ERR() << "Debug b/249457381 RendererVk::onDestroy():" << __LINE__ << " this:" << this;
     if (isDeviceLost())
     {
         handleDeviceLost();
@@ -1456,7 +1455,6 @@ angle::Result RendererVk::initialize(DisplayVk *displayVk,
                                      const char *wsiExtension,
                                      const char *wsiLayer)
 {
-    ERR() << "Debug b/249457381 RendererVk::initialize():" << __LINE__ << " this:" << this;
     bool canLoadDebugUtils = true;
 #if defined(ANGLE_SHARED_LIBVULKAN)
     {
